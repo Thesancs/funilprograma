d2ff61920e852cbb95a8866ae2f84ecbf1990663
Glauber Santos <Santos.Glauber2002@gmail.com>
2025-07-19 19:38:20 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Card className="w

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -1,10 +1,8 @@
 import QuizGravidez from '@/components/funnel/QuizGravidez';
-import QuizTrimestre from '@/components/funnel/QuizTrimestre';
 
 export default function QuizPage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#D9A8B6] to-background p-4 gap-8">
-      <QuizTrimestre />
       <QuizGravidez />
     </main>
   );
